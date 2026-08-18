1f8c2bb46b2a2fa5da35293261d6efe38b0a3f66
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-08-18 07:41:56 -0300

    test: add unit tests for GetServicesQueryHandle

---
+++ tests/PetClinix.UnitTests/Handlers/GetServicesQueryHandlerTests.cs
@@ -0,0 +1,73 @@
+using FluentAssertions;
+using NSubstitute;
+using PetClinix.Modules.Catalog.Application.UseCases.GetServices;
+using PetClinix.Modules.Catalog.Domain.Entities;
+using PetClinix.Modules.Catalog.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PetClinix.UnitTests.Handlers;
+
+public class GetServicesQueryHandlerTests
+{
+    private readonly IServiceRepository _serviceRepositoryMock;
+    private readonly GetServicesQueryHandler _handler;
+
+    public GetServicesQueryHandlerTests()
+    {
+        _serviceRepositoryMock = Substitute.For<IServiceRepository>();
+        _handler = new GetServicesQueryHandler(_serviceRepositoryMock);
+    }
+
+    private static Service CreateValidService(Guid clinicId)
+    {
+        return Service.Create(
+            clinicId, Guid.NewGuid(), "Consulta", "Consulta Geral", 30, 150.0m, true
+        );
+    }
+
+    [Fact]
+    public async Task Handle_Should_Return_PagedResult_With_Correct_Data()
+    {
+        var clinicId = Guid.NewGuid();
+        var query = new GetServicesQuery(clinicId, 1, 10);
+
+        var services = new List<Service>
+        {
+            CreateValidService(clinicId),
+            CreateValidService(clinicId)
+        };
+
+        _serviceRepositoryMock.GetAllByClinicIdAsync(clinicId, 1, 10, Arg.Any<CancellationToken>())
+            .Returns((services, 2));
+
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value!.Items.Should().HaveCount(2);
+        result.Value.TotalCount.Should().Be(2);
+        result.Value.PageNumber.Should().Be(1);
+        result.Value.PageSize.Should().Be(10);
+    }
+
+    [Fact]
+    public async Task Handle_Should_Return_Empty_List_When_No_Services_Exist()
+    {
+        var clinicId = Guid.NewGuid();
+        var query = new GetServicesQuery(clinicId, 1, 10);
+
+        _serviceRepositoryMock.GetAllByClinicIdAsync(clinicId, 1, 10, Arg.Any<CancellationToken>())
+            .Returns((new List<Service>(), 0));
+
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value!.Items.Should().BeEmpty();
+        result.Value.TotalCount.Should().Be(0);
+    }
+}
